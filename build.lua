131a217ea42ba516c68e0766436bdf533414031d
Ben Crist <ben@magicmoremagic.com>
2017-01-08 15:53:39 -0600

    build.lua syntax overhaul

--- build.lua
+++ build.lua
@@ -1,18 +1,12 @@
-module { name = 'cli',
-   projects = {
-      lib {
-         limp = {
-            'include/section_ids.hpp'
-         },
-         src = {
-            'src/*.cpp',
-            'src/native/*.cpp',
-            'src/native/' .. toolchain .. '/*.cpp',
-            pch = 'src/pch.cpp'
-         },
-         preprocessor = {
-            'BE_CLI_IMPL'
-         }
-      }
+module 'cli' {
+   lib {
+      limp 'include/section_ids.hpp',
+      src {
+         'src/*.cpp',
+         'src/native/*.cpp',
+         'src/native/$(toolchain)/*.cpp',
+      },
+      pch_src 'src/pch.cpp',
+      define 'BE_CLI_IMPL'
    }
 }
